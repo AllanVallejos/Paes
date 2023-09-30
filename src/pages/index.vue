3ca7b60b122dd73388f359d43e0b2768f27c6fe5
ArturoAlvarezz <arturoalvareezz@gmail.com>
2023-09-30 19:54:27 -0300

    paginas y componentes basicos para login

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,12 @@
+import { NuxtLink } from '../../.nuxt/components';
 <template>
+    <main>
+    </main>
     <div>
       <h1>My Nuxt.js App!</h1>
+        <NuxtLink to="/admin">Admin</NuxtLink>
+        <NuxtLink to="/login">Login</NuxtLink>
+        <NuxtLink to="/account">Account</NuxtLink>
     </div>
 </template>
   
---
+++ src/pages/login.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>
+        <PageTitle title="Login page" />
+        <PageDescription description="This page should only be accessible to guests." />
+        <div>
+            <h1>Login page</h1>
+            <p>This page should only be accessible to guests.</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+
+
+<style>
+
+</style>
