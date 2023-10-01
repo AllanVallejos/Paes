ee680b686948bb64d945f260bcaad6e5715c1aea
ArturoAlvarezz <arturoalvareezz@gmail.com>
2023-10-01 16:18:33 -0300

    Supabase funcionando con ejemplo base

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,26 @@
 import { NuxtLink } from '../../.nuxt/components';
+
+<script setup>
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('https://dtwternontlvmanfkges.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0d3Rlcm5vbnRsdm1hbmZrZ2VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYxMjQ2MTgsImV4cCI6MjAxMTcwMDYxOH0.zwfrotgKRisKCON_PfTyJ8ZmDq6wwjtDQSLEpyLfRcI')
+const countries = ref([])
+
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = data
+}
+
+onMounted(() => {
+  getCountries()
+})
+</script>
+
 <template>
+  <ul>
+    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+  </ul>
+</template>
+<!-- <template>
     <main>
     </main>
     <div>
@@ -8,5 +29,5 @@ import { NuxtLink } from '../../.nuxt/components';
         <NuxtLink to="/login">Login</NuxtLink>
         <NuxtLink to="/account">Account</NuxtLink>
     </div>
-</template>
+</template> -->
   
