cb6669313de6f6531d9e6f32ffc57f896045579f
Matias Marquez San Martin <marquez-514@hotmail.com>
2023-10-14 22:41:52 -0300

    Se arreglo el footer

--- pages/foro.vue
+++ pages/foro.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <body style="min-height: 86vh; position: relative; padding-bottom: 5rem;">
+    <body>
+
         <h1>Foro</h1>
+
     </body>
+    
   </div>
 </template>
 
